016ab0925b83b0d46610df4314c65043c439287e
Mahesh Sangawar <mahesh.sangawar@gmail.com>
2017-12-05 05:17:31 +0000

    Table.js edited online with Bitbucket

--- src/components/common/Table.js
+++ src/components/common/Table.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import TableRow from './TableRow';
 
-const Table = (data) => (
-  <table>
-    {data.map(row => {
-      <TableRow row={row} />;
-    })}
-  </table>
-);
+
+class Table extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>makeId</th>
+            <th>Name</th>
+            <th>price</th>
+          </tr>
+        </thead>
+      <tbody>
+        {this.props.data.map(({ id, makeId, name, price, imageUrl}) =>
+        <tr key={id}>
+          <td>{makeId}</td>
+          <td>{name}</td>
+          <td>{price}</td>
+        </tr>
+      )}
+      </tbody>
+      </table>
+    );
+  }
+}
 
 export default Table;
